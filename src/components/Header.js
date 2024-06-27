91757f6ea03e6623ecd77750903876b5ffff7b20
Miguelpdev <pablo.espinoza_11@icloud.com>
2024-06-27 18:44:35 -0500

    add campus page mobile design

--- src/components/Header.js
+++ src/components/Header.js
@@ -211,7 +211,7 @@ export default function Example() {
                   )}
                 </Disclosure>
                 <Link
-                  to="#"
+                  to="/campus"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Campus
